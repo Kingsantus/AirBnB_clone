7276cf9733fee3b949f980eb7b624b9e4eb966b5
kingsantus <kingsantusasogwa@gmail.com>
2023-10-13 03:54:22 -0700

    Console 0.1

--- console.py
+++ console.py
@@ -1,9 +1,42 @@
 #!/usr/bin/python3
 """console module that accept input and process"""
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class help process cmd input"""
     prompt = '(hbnb) '
+    __classes = [
+            "BaseModel"
+            ]
+
+    def do_create(self, arg):
+        """Create a new instance of basemodel, save it and print"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance  = eval(args[0] + '()')
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key in models.storage.all():
+                print(models.storage.all()[key])
+            else:
+                print("** no instance found **")
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
