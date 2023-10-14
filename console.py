88d37c746cde032e74d39f8cb67a783b37bf7013
kingsantus <kingsantusasogwa@gmail.com>
2023-10-14 02:53:36 -0700

    Console 0.1

--- console.py
+++ console.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """console module that accept input and process"""
 import cmd
+import models
 from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class help process cmd input"""
@@ -27,16 +29,47 @@ class HBNBCommand(cmd.Cmd):
         args = arg.split()
         if len(args) == 0:
             print("** class name missing **")
-        elif args[0] not in self.__classes:
+        elif args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         elif len(args) == 1:
             print("** instance id missing **")
         else:
             key = args[0] + "." + args[1]
-            if key in models.storage.all():
-                print(models.storage.all()[key])
+            all_objs = models.storage.all()
+            if key in all_objs:
+                print(all_objs[key])
             else:
                 print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Method that delete an instance using the class name and id"""
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            objects = models.storage.all()
+            key = args[0] + "." + args[1]
+            if key in objects.keys():
+                objects.pop(key, None)
+                models.storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """method print all string repe of all instance based or not on the class"""
+        args = arg.split()
+
+        obj_list = []
+        if len(args) == 0 or args[0] in HBNBCommand.__classes:
+            for objects in models.storage.all().values():
+                obj_list.append(str(obj))
+            print(obj_list)
+        else:
+            print("** class doesn't exist **")
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
