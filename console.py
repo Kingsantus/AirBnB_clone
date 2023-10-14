c632e5b9945c12120aaf809a43fd3cb9de8ccbc9
kingsantus <kingsantusasogwa@gmail.com>
2023-10-14 05:04:16 -0700

    Console 1.0

--- console.py
+++ console.py
@@ -4,12 +4,24 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class help process cmd input"""
     prompt = '(hbnb) '
     __classes = [
-            "BaseModel"
+            "BaseModel",
+            "User",
+            "State",
+            "City",
+            "Amenity",
+            "Place",
+            "Review"
             ]
 
     def do_create(self, arg):
